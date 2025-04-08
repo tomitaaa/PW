a836f97d9b260041bedacb8242aea9d034ae2e4a
Tomita <gabrieltomita@hotmail.com>
2025-04-08 11:43:47 -0300

    adicionado botão de light/dark
    criação do botão funcional (por enquanto dentro da box)

--- js/index.js
+++ js/index.js
@@ -1,6 +1,23 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+  const temaLocal = localStorage.getItem("tema");
+  document.body.setAttribute("data-theme", temaLocal);
+});
+
 function openTab(tabName) {
   let contents = document.querySelectorAll(".tab-content");
   contents.forEach((content) => content.classList.remove("active"));
 
   document.getElementById(tabName).classList.add("active");
+}
+
+function alterarTema() {
+  const tema = document.body.getAttribute("data-theme");
+  const novoTema = tema == "light" ? "dark" : "light";
+  document.body.setAttribute("data-theme", novoTema);
+
+  const btAlterarTema = document.getElementById("btAlterarTema");
+  btAlterarTema.textContent = "Light";
+  if (novoTema == "light") {
+    btAlterarTema.textContent = "Dark";
+  }
 }
