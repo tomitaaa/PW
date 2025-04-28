e90ec36a214639487e47a99fc4a3ead8679a1c1b
Tomita <gabrieltomita@hotmail.com>
2025-04-28 17:00:31 -0300

    botão de idioma pro parte do sobre

--- js/index.js
+++ js/index.js
@@ -3,6 +3,39 @@ document.addEventListener("DOMContentLoaded", (event) => {
   document.body.setAttribute("data-theme", temaLocal);
 });
 
+let idiomaAtual = "pt";
+
+function alterarIdioma() {
+  idiomaAtual = idiomaAtual == "pt" ? "en" : "pt";
+  carregarIdioma(idiomaAtual);
+}
+function carregarIdioma(idioma) {
+  fetch(`json/${idioma}.json`)
+    .then((data) => data.json())
+    .then((data) => {
+      traduzirPagina(data);
+    });
+
+  function traduzirPagina(Linguagem) {
+    document.querySelectorAll("[data-i18n]").forEach((elemento) => {
+      const chave = elemento.getAttribute("data-i18n");
+
+      if (Linguagem[chave]) {
+        elemento.textContent = Linguagem[chave];
+      }
+    });
+
+    document.querySelectorAll("[data-i18n-alt]").forEach((elemento) => {
+      const chave = elemento.getAttribute("data-i18n-alt");
+
+      if (Linguagem[chave]) {
+        // elemento.textContent = Linguagem[chave];
+        elemento.setAttribute("alt", Linguagem[chave]);
+      }
+    });
+  }
+}
+
 function openTab(tabName) {
   let contents = document.querySelectorAll(".tab-content");
   contents.forEach((content) => content.classList.remove("active"));
@@ -14,10 +47,9 @@ function alterarTema() {
   const tema = document.body.getAttribute("data-theme");
   const novoTema = tema == "light" ? "dark" : "light";
   document.body.setAttribute("data-theme", novoTema);
-
+  localStorage.setItem("tema", novoTema); 
   const btAlterarTema = document.getElementById("btAlterarTema");
-  btAlterarTema.textContent = "Light";
-  if (novoTema == "light") {
-    btAlterarTema.textContent = "Dark";
-  }
+  btAlterarTema.textContent = novoTema === "light" ? "Modo Dark" : "Modo Light";
 }
+
+
